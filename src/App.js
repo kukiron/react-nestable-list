a8755f5b04c7edc1a65b44484009e4baca81d8b3
kukiron <kafiluddin.kiron@gmail.com>
2018-10-01 16:26:48 +0600

    Enable loading & removing json data from localstorage

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react"
+import { connect } from "react-redux"
+
+import "./App.css"
+import NestableList from "./features/NestableList"
+import LoadJson from "./features/LoadJson"
+import { removeList } from "./actions"
+import { loadFromLocalStorage } from "./tools/utils"
+
+class App extends Component {
+  state = {
+    list: loadFromLocalStorage()
+  }
+
+  handleListChange = list => this.setState({ list })
+
+  isCollapsed = () => {
+    const form = document.forms[0] || null
+    return form && form.elements["collapsed"].checked
+  }
+
+  renderItem = ({ item, collapseIcon, handler }) => {
+    return (
+      <div className="drag-item">
+        {handler}
+        {collapseIcon}
+        <b>{item.text}</b>: <span>{item.position}</span>
+      </div>
+    )
+  }
+
+  // remove list
+  handleClick = () => {
+    this.props.removeList()
+    setTimeout(() => {
+      this.setState({ list: this.props.employeeList })
+    }, 0)
+  }
+
+  render() {
+    const items = this.state.list
+
+    return (
+      <div>
+        <h3 className="heading text-center py-3">Company Organigram</h3>
+        <hr />
+        <LoadJson onListChange={this.handleListChange} />
+        <br />
+        <div>
+          {items.length > 0 ? (
+            <div>
+              <NestableList items={items} renderItem={this.renderItem} />
+              <button
+                className="btn btn-raised btn-danger w-100 d-flex justify-content-center align-items-center my-5 mx-auto"
+                onClick={this.handleClick}
+              >
+                Clear List
+                <i className="ml-2 material-icons">delete</i>
+              </button>
+            </div>
+          ) : (
+            <h1 className="text-center p-5">Upload JSON</h1>
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return { employeeList: state.employeeList }
+}
+
+export default connect(
+  mapStateToProps,
+  { removeList }
+)(App)
